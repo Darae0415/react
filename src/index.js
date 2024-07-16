81fb94741367ddc33c549e4cc66065526b78eae1
Darae0415 <ekfogksmf@naver.com>
2024-07-16 17:38:42 +0900

    0716오후

--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
 import ReactDOM from 'react-dom/client';
-import {App2} from './App'
-// const App = ()=>{
-//   return (
-//     <p>안녕하세요</p>
-//   );
-// }
-// function App(){
-//   const divStyle = {
-//     border: '1px solid blue'
-//   }
-//   return(
-//     <div style={divStyle}>
-//       <p>안녕하세요</p>
-//       <p>반갑습니다.</p>
-//     </div>
-//   )
-// }
+import {App} from './App'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <App2/>
+    <App/>
   </>
 );
 
