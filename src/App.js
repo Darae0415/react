61e9f1c50b0d01b1196f5325ff6b0468f3ec0a55
Darae0415 <ekfogksmf@naver.com>
2024-07-16 15:39:22 +0900

    리액트 사용방법

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <ul>
-        <li>강아지</li>
-        <li>토끼</li>
-        <li>고양이</li>
-        <li>거북이</li>
-      </ul>
-      <table border="1px">
-        <tr>
-          <td>이름</td>
-          <td>나이</td>
-        </tr>
-        <tr>
-          <td>홍길동</td>
-          <td>24</td>
-        </tr>
-        <tr>
-          <td>이순신</td>
-          <td>30</td>
-        </tr>
-     </table>
-    </div>
-  );
-}
-
-export default App;
+export function App2(){
+    const divStyle = {
+      border: '1px solid blue'
+    }
+    return(
+      <div style={divStyle}>
+        <p>안녕하세요</p>
+        <p>반갑습니다.</p>
+      </div>
+    )
+  }
